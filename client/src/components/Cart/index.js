4d00887deb4f2f144c5f605e889a52ac60c5fd8d
Charanvir <charanvir123@gmail.com>
2022-07-13 17:02:59 -0400

    currently implemeting stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,17 @@ import './style.css'
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers'
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js'
+import { useLazyQuery } from '@apollo/client';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+
+    // data will contain the checkout dession, but only after the query is called with the getCheckout function
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -34,6 +42,20 @@ const Cart = () => {
         return sum.toFixed(2)
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        })
+    }
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -57,7 +79,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
