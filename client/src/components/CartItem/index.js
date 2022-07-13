3703a9d80eb1a1c6c0cb306fc7ccb81a77b2cd48
Charanvir <charanvir123@gmail.com>
2022-07-13 02:57:02 -0400

    implemented IndexedDB to shopping cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -4,6 +4,7 @@ import {
     REMOVE_FROM_CART,
     UPDATE_CART_QUANTITY
 } from '../../utils/actions'
+import { idbPromise } from '../../utils/helpers'
 
 const CartItem = ({ item }) => {
     // no need for state because item is passed down from Cart
@@ -14,6 +15,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item })
     };
 
     const onChange = (e) => {
@@ -23,12 +25,14 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             })
+            idbPromise('cart', 'delete', { ...item })
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             })
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) })
         }
     }
 
